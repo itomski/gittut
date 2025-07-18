5fa95ad08db1c4a9558e89cb99f5f8190fd4b980
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-07-18 13:57:56 +0200

    App um ein for-Loop erweiter

--- src/main/java/de/lubowiecki/gittut/App.java
+++ src/main/java/de/lubowiecki/gittut/App.java
@@ -4,5 +4,9 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hallo GIT");
+
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(i);
+        }
     }
 }
