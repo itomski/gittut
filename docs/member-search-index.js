81b5ffbaad4eedb5e9ca149dd33ccb37ecacae08
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-07-18 14:24:48 +0200

    Ganz tolle Änderungen

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"de.lubowiecki.gittut","c":"App","l":"App()","u":"%3Cinit%3E()"},{"p":"de.lubowiecki.gittut","c":"App","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"de.lubowiecki.gittut"}];updateSearchResults();
